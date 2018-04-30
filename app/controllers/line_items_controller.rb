4a3e1917c780cea333922f4cda1e409c23df17f0
jared clemmensen <jaredclemmensen@gmail.com>
2018-04-30 13:29:04 -0700

    add item removal from order

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -8,6 +8,14 @@ class LineItemsController < ApplicationController
     redirect_to products_path
   end
 
+  def destroy
+    @order = current_order
+    @item = @order.line_items.find(params[:id])
+    @item.destroy
+    @order.save
+    redirect_to order_path
+  end
+
   private
 
   def item_params
