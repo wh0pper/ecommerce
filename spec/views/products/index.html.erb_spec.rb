80beaed55ad1a46aab9bf7442a3135b0ddf0f027
jared clemmensen <jaredclemmensen@gmail.com>
2018-04-30 14:25:40 -0700

    comment out unecessary tests generated by scaffolding

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "products/index", type: :view do
-  before(:each) do
-    assign(:products, [
-      Product.create!(
-        :name => "Name",
-        :price => "",
-        :price => ""
-      ),
-      Product.create!(
-        :name => "Name",
-        :price => "",
-        :price => ""
-      )
-    ])
-  end
-
-  it "renders a list of products" do
-    render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
-  end
-end
+# RSpec.describe "products/index", type: :view do
+#   before(:each) do
+#     assign(:products, [
+#       Product.create!(
+#         :name => "Name",
+#         :price => "10.00"
+#       ),
+#       Product.create!(
+#         :name => "Name",
+#         :price => "10.00"
+#       )
+#     ])
+#   end
+#
+#   it "renders a list of products" do
+#     render
+#     assert_select "tr>td", :text => "Name".to_s, :count => 2
+#     assert_select "tr>td", :text => "10.00".to_s, :count => 2
+#   end
+# end
