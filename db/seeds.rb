249641236c65ea36f410de8ada034558bcc8688a
jared clemmensen <jaredclemmensen@gmail.com>
2018-04-30 10:20:13 -0700

    add product db seeding with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+20.times do |index|
+  Product.create!(name: Faker::Commerce.product_name, price: Faker::Commerce.price);
+end
+
+p "Created #{Product.count} products
