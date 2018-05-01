d2669798e7b7a309c6ed2fef60c6d73cf8d6e8b6
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-01 11:27:55 -0700

    add order history show link

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,18 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, :except => :show
 
   def show
-    @order_items = current_order.line_items
+    if params[:format]
+      @order = Order.find(params[:format])
+    else
+      @order = current_order
+    end
   end
 
   def update
-    # current_order.status = "Placed"
-    # current_order.placed_on = Date.today
-    # current_order.save
     current_order.update(status: "Placed", placed_on: Date.today)
     flash[:notice] = "Order successfully placed."
     new_order = Account.where(user_id: current_user.id).first.orders.create
     session[:order_id] = new_order.id
-    binding.pry
     redirect_to products_path
   end
 end
